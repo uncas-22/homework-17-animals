ebc1ad323dd66b1ec599af17f88920618a69170d
Alex <seredin-alex@mail.ru>
2022-12-08 21:01:39 +0300

    Созданы классы Animals end Birds

--- src/animals/Animals.java
+++ src/animals/Animals.java
@@ -1,4 +1,58 @@
 package animals;
+package animals;
+
+import java.util.Objects;
 
 public class Animals {
-}
+    private String name;
+    private int age;
+} else {
+        this.age = 0;
+        }
+        }
+
+public String getName() {
+        return name;
+public void go() {
+
+        }
+
+public void sleep() {
+
+        }
+
+public void eat() {
+
+        }
+
+public String getNameAnimals() {
+        return nameAnimals;
+        }
+
+public void setName(String name) {
+        if (name != null && !name.isBlank() && !name.isEmpty()) {
+        this.name = name;
+        }
+        }
+
+public int getAge() {
+        return age;
+        }
+
+@Override
+public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animals animals = (Animals) o;
+        return ageAnimals == animals.ageAnimals && Objects.equals(nameAnimals, animals.nameAnimals);
+        }
+
+@Override
+public int hashCode() {
+        return Objects.hash(nameAnimals, ageAnimals);
+        }
+
+public String toString() {
+        return "Кличка -" + getNameAnimals() + ", возраст: " + getAgeAnimals();
+        }
+        }
