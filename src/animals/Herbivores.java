ead6b0efb21d48196eb7496745ae55b70f70a09d
Alex <seredin-alex@mail.ru>
2022-12-08 21:07:04 +0300

    Созданы классы Herbivores, Mammals end Predators

--- src/animals/Herbivores.java
+++ src/animals/Herbivores.java
@@ -1,4 +1,66 @@
 package animals;
+import java.util.Objects;
 
-public class Herbivores {
+public class Herbivores extends Mammals {
+
+    private String typeOfFood;
+
+    public Herbivores(String nameAnimals, int ageAnimals, String habitat, int speedOfMoving, String typeOfFood) {
+        super(nameAnimals, ageAnimals, habitat, speedOfMoving);
+        setTypeOfFood(typeOfFood);
+    }
+
+    public void go() {
+        System.out.println("По открытой местности");
+
+    }
+
+    public void sleep() {
+        System.out.println("В основном в ночное время");
+
+    }
+
+    public void eat() {
+        System.out.println("Большая часть рациона - растения.");
+
+    }
+
+    public void walk() {
+        System.out.println("В поисках еды");
+    }
+
+    public void graze() {
+        System.out.println("Жуют свежую сочную зелень");
+    }
+
+    public String getTypeOfFood() {
+        return typeOfFood;
+    }
+
+    public void setTypeOfFood(String typeOfFood) {
+        if (typeOfFood != null && !typeOfFood.isBlank() && !typeOfFood.isEmpty()) {
+            this.typeOfFood = typeOfFood;
+        } else {
+            this.typeOfFood = "зеленые растения, сочная трава";
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Herbivores that = (Herbivores) o;
+        return Objects.equals(typeOfFood, that.typeOfFood);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), typeOfFood);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Основная пища: " + getTypeOfFood();
+    }
 }
--- src/animals/Predators.java
+++ src/animals/Predators.java
@@ -1,4 +1,66 @@
 package animals;
+import java.util.Objects;
 
-public class Predators {
+public class Predators extends Mammals {
+
+    private String typeFood;
+
+    public Predators(String nameAnimals, int ageAnimals, String habitat, int speedOfMoving, String typeFood) {
+        super(nameAnimals, ageAnimals, habitat, speedOfMoving);
+        setTypeFood(typeFood);
+    }
+
+    public void go() {
+        System.out.println("По открытой местности");
+
+    }
+
+    public void sleep() {
+        System.out.println("В основном в ночное время");
+
+    }
+
+    public void eat() {
+        System.out.println("Большая часть рациона - мясо.");
+
+    }
+
+    public void walk() {
+        System.out.println("В поисках еды");
+    }
+
+    public void hunt() {
+        System.out.println("Преследуют жертву и выжидают удобный момент для нападения");
+    }
+
+    public String getTypeFood() {
+        return typeFood;
+    }
+
+    public void setTypeFood(String typeFood) {
+        if (typeFood != null && !typeFood.isBlank() && !typeFood.isEmpty()) {
+            this.typeFood = typeFood;
+        } else {
+            this.typeFood = "тушки животных";
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Predators predators = (Predators) o;
+        return Objects.equals(typeFood, predators.typeFood);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), typeFood);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Основная пища: " + getTypeFood();
+    }
 }
