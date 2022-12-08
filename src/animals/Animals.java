eec799ae123f7e15c04409f53ac5a9e9bcebac3a
Alex <seredin-alex@mail.ru>
2022-12-08 21:11:44 +0300

    Доработка класса Animals

--- src/animals/Animals.java
+++ src/animals/Animals.java
@@ -1,58 +1,61 @@
 package animals;
-package animals;
 
 import java.util.Objects;
 
 public class Animals {
-    private String name;
-    private int age;
-} else {
-        this.age = 0;
+    private String nameAnimals;
+    private int ageAnimals;
+    public Animals (String name, int age) {
+        setName(name);
+        if (age > 0) {
+            this.age = age;
+} else{
+        this.age=0;
         }
         }
 
-public String getName() {
+public String getName(){
         return name;
-public void go() {
+public void go(){
 
         }
 
-public void sleep() {
+public void sleep(){
 
         }
 
-public void eat() {
+public void eat(){
 
         }
 
-public String getNameAnimals() {
+public String getNameAnimals(){
         return nameAnimals;
         }
 
-public void setName(String name) {
-        if (name != null && !name.isBlank() && !name.isEmpty()) {
-        this.name = name;
+public void setName(String name){
+        if(name!=null&&!name.isBlank()&&!name.isEmpty()){
+        this.name=name;
         }
         }
 
-public int getAge() {
+public int getAge(){
         return age;
         }
 
 @Override
-public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Animals animals = (Animals) o;
-        return ageAnimals == animals.ageAnimals && Objects.equals(nameAnimals, animals.nameAnimals);
+public boolean equals(Object o){
+        if(this==o)return true;
+        if(o==null||getClass()!=o.getClass())return false;
+        Animals animals=(Animals)o;
+        return ageAnimals==animals.ageAnimals&&Objects.equals(nameAnimals,animals.nameAnimals);
         }
 
 @Override
-public int hashCode() {
-        return Objects.hash(nameAnimals, ageAnimals);
+public int hashCode(){
+        return Objects.hash(nameAnimals,ageAnimals);
         }
 
-public String toString() {
-        return "Кличка -" + getNameAnimals() + ", возраст: " + getAgeAnimals();
+public String toString(){
+        return"Кличка -"+getNameAnimals()+", возраст: "+getAgeAnimals();
         }
         }
